9b3945d4fa328eee69871902a8da8681507e59e5
mullenlucas <mullenlucas@gmail.com>
2022-11-30 19:56:28 -0300

    fix comments to reference author

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_184135) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_225447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.bigint "post_id", null: false
+    t.string "text"
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -53,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_184135) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
