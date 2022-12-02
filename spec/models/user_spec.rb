82239d029ea8be93e3b429e25d5dd39b71738d43
mullenlucas <mullenlucas@gmail.com>
2022-12-02 17:34:35 -0300

    add user testing

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.create(
+    name: 'Taylor', 
+    photo: 'https://img.olympicchannel.com/images/image/private/f_auto/t_1-1_300/primary/wfrhxc0kh2vvq77sonki', 
+    bio: 'Football player',
+    posts_counter: 9
+    ) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:bio) }
+    it { should validate_numericality_of(:posts_counter) }
+    it 'photo value is a string' do
+      subject.photo = 'https://img.olympicchannel.com/images/image/private/f_auto/t_1-1_300/primary/wfrhxc0kh2vvq77sonki'
+      expect(subject).to be_valid
+    end
+    it 'posts_counter valur to be an integer' do
+      subject.posts_counter = 9
+      expect(subject).to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it { should have_many(:posts) }
+    it { should have_many(:likes) }
+    it { should have_many(:comments) }
+  end
+
+end
