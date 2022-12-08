e22f3deaf287a21ff21ee9cf0a836f2d2bba001b
mullenlucas <mullenlucas@gmail.com>
2022-12-08 16:32:18 -0300

    add users_spec tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'users/show'
   get 'users/index'
   root 'users#index'
   # Defines the root path route ("/")
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,41 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    before(:each) do 
+      get users_path
+    end
+
     it 'returns http success' do
-      get '/users/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template' do
+      expect(response).to render_template(:index) 
+    end
+
+    it 'includes placeholder text' do
+      expect(response.body).to include('Here is the list of users:')
     end
   end
 
   describe 'GET /show' do
+    before(:each) do 
+      @user = User.create(name: 'Scaloni', 
+        photo: 'https://www.cronista.com/files/image/486/486079/63626859b6090_360_480!.jpg?s=93b47f953e7bfcf7bd1ca5126d324c0a&d=1667405345', 
+        bio: 'Football player, coach and Ruby on Rails developer')
+      get user_path(@user.id)
+    end 
+
     it 'returns http success' do
-      get '/users/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template' do
+      expect(response).to render_template(:show) 
+    end
+
+    it 'includes placeholder text' do
+      expect(response.body).to include(@user.bio)
     end
   end
 end
