7478c4dd569f1d98dfbd04ebbb523e0b7330d011
mullenlucas <mullenlucas@gmail.com>
2022-12-08 16:15:17 -0300

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'users/show'
+  get 'users/index'
+  root 'users#index'
+  # Defines the root path route ("/")
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
-  # Defines the root path route ("/")
-  root "users#index"
-  # root "articles#index"
 end
