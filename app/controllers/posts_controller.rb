44d1c9498d2544059d45e27b8645aa61286eac7f
mullenlucas <mullenlucas@gmail.com>
2022-12-08 22:29:49 -0300

    implementing fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:author_id])
+    @user = User.find(params[:name])
   end
 
   def show
-    @user = User.find(params[:author_id])
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
 end
---
+++ db/migrate/20221208205336_change_nil_to_zero.rb
@@ -0,0 +1,8 @@
+class ChangeNilToZero < ActiveRecord::Migration[7.0]
+  def change
+    change_column :posts, :likes_counter, :integer, default: 0, null: false
+    change_column :posts, :comments_counter, :integer, default: 0, null: false
+
+    change_column :users, :posts_counter, :integer, default: 0, null: false
+  end
+end
