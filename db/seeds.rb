928219020beba6a61dafb240fe87c639770319a2
mullenlucas <mullenlucas@gmail.com>
2022-12-09 14:31:48 -0300

    add styling to users and dummy for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,20 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.delete(User.all)
+# User.delete(User.all)
 
-User.create(name: 'Zaccarias',
-  photo: 'https://unsplash.com/photos/tAvpDE7fXgY',
-  bio: 'Graphic Designer and Ruby on Rails educator')
+# User.create(name: 'Zaccarias',
+#   photo: 'https://unsplash.com/photos/tAvpDE7fXgY',
+#   bio: 'Graphic Designer and Ruby on Rails educator')
 
-User.create(name: 'Shydra',
-  photo: 'https://unsplash.com/photos/i2hoD-C2RUA',
-  bio: 'Train smasher and Rails developer')
+# User.create(name: 'Shydra',
+#   photo: 'https://unsplash.com/photos/i2hoD-C2RUA',
+#   bio: 'Train smasher and Rails developer')
 
-User.create(name: 'Eustela',
-  photo: 'https://unsplash.com/photos/_H6wpor9mjs',
-  bio: 'Guardian of the bakes and Ruby on Rails philosopher')
+# User.create(name: 'Eustela',
+#   photo: 'https://unsplash.com/photos/_H6wpor9mjs',
+#   bio: 'Guardian of the bakes and Ruby on Rails philosopher')
+
+Post.create(title: 'First Post', text: 'This is the first post', author_id: 1)
+Post.create(title: 'Second Post', text: 'This is the second post', author_id: 1)
+Post.create(title: 'Third Post', text: 'This is the third post', author_id: 1)
