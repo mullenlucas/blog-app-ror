0f4ff9497146b1f5b2a48ddf21ffd5c2a6b6ed41
mullenlucas <mullenlucas@gmail.com>
2022-12-21 18:51:42 -0300

    add devise migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   def current_user
     User.find(params[:user_id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in   
   get 'users/index'
   root 'users#index'
