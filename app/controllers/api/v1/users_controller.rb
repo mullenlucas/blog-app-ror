08a1bd5cd01bbbfce7fdc89eba1eac25f145a452
mullenlucas <mullenlucas@gmail.com>
2022-12-22 15:48:57 -0300

    add controllers for api v1

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+end
